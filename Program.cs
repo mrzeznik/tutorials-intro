c6c24a6ca5469befbfa15b118eb941ff3455a6e3
Michal Rzeznik <90emer@gmail.com>
2022-04-29 15:26:10 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HelloWorld
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                var upperText = args[i].ToUpper();
+
+                System.Console.WriteLine(upperText);
+            }
+
+            System.Console.WriteLine("Hello, World!");
+
+            return;
+        }
+    }
+}
