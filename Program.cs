c5696703c6bf0dc2797e622c5feacea8500d525b
Michal Rzeznik <90emer@gmail.com>
2022-04-29 16:14:55 +0200

    init mouse with args

--- Program.cs
+++ Program.cs
@@ -7,9 +7,17 @@ namespace HelloWorld
         public static void Main(string[] args)
         {
             // var upperTextArrayArgs = TextModifications.MakeArrayTextUpper(args);
-            
+
             // create a new Mouse
-            var mouseToCheck = new Mouse();
+            var mouseToCheck = new Mouse()
+            {
+                Brand = args[0],
+                IsWireless = bool.Parse(args[1]),
+                NumberOfButtons = int.Parse(args[2]),
+                Color = args[3],
+                HasLight = bool.Parse(args[4]),
+                Price = decimal.Parse(args[5])
+            };
 
             return;
         }
